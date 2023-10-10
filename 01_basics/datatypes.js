f30ac1677e4157c59a0c98ee17e482a57987034c
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-10 17:05:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+// Datatypes:Number,String,Boolean,Null,Undefined,Symbol
+// Null : It is a standalone value 
+let name="yatendra singh";
+let age=18;
+let isLoggedIn=true;
+let state=null;//Empty
+let userEmail;//Undefined
+// Symbol : Unique
+console.log(typeof(userEmail));
+
+
