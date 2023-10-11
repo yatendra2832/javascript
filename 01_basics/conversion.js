081b6120d0d9c20c11e3ff12ca4719ba10deff16
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-11 02:54:22 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,11 @@
+let score="33";
+// console.log(typeof(score));
+// let numberIn=Number(score);
+// console.log(typeof numberIn);
+
+let isLoggedIn="hello";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=>true 0=>false
+// ""=>false "hello"=>true
