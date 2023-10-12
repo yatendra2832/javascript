3dc57998c3c48f840a2e0d0791a79303fcee2104
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-12 19:36:04 +0000

    conversion of datatype

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -2,10 +2,21 @@ let score="33";
 // console.log(typeof(score));
 // let numberIn=Number(score);
 // console.log(typeof numberIn);
-
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1 false=>0
+*/
 let isLoggedIn="hello";
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=>true 0=>false
 // ""=>false "hello"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);//that will simply print the number 33 on the screen
+console.log(typeof stringNumber);//the number will become the string now 
+
+
