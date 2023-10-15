555e945e513cb19d323c054cb19ae44718ceb9d8
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-15 08:34:17 +0000

    date&time

---
+++ 01_basics/date_and_time.js
@@ -0,0 +1,42 @@
+// Dates
+// Month will start from the 0 to 11 means 0->january 1->february 2->march ...11->December
+/*
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toString())
+
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,10,23)
+let myCreatedDate = new Date("2023-01-14")
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
+*/
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp)
+// let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000))
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.getFullYear())
+// console.log(myDate.getMonth())
+// console.log(myDate.getDay())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
