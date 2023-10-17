f08a8ac535914e7175b3402c57318fdc096106b4
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-17 09:36:00 +0000

    function scopes

---
+++ 04_functions/function3.js
@@ -0,0 +1,15 @@
+// Scopes in Function curly braces are the scope it can be with the function,if-else at that timeit is known as scope of that particular function
+
+if(true){
+    let   a=10
+    const b=20
+    var   c=30
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+
+
