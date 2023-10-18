1908269e770eaf1fdd90071c996a8deff80692bc
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-18 06:09:17 +0000

    arrow function

--- 04_functions/function5.js
+++ 04_functions/function5.js
@@ -6,9 +6,43 @@ const user={
     website:"www.yadavjiediting.com",
     welcomeMessage:function () {
         console.log(`${this.username} , Welcome to Website ${this.website}`);
+        console.log(this)
     }
 }
 // here this refers to the user object 
+// user.welcomeMessage();
+// user.username="Yatendra Yadav"
+// user.welcomeMessage();//that will use the username as the yatendra yadav as we replace it with the name yatendra singh
+// console.log(this)// that will return an empty object but in the browser this will refer to the window
+
+
+function thisFunction(params) {
+    console.log(this);
+}
+// thisFunction();
+
+// Arrow Function
+ const arrowFunction = () => {
+    let username="Yatendra Singh"
+    console.log(this.username)
+ }
+//  arrowFunction();
+
+// const addTwo = (num1,num2)=>{
+//     return num1+num2
+// }
+// Implicit return
+const addTwo = (num1,num2)=> num1+num2
+
+
+console.log(addTwo(3,4));
+
+
+
+
+
+
+
 
 
 
