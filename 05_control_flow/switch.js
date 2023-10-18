58ea170e0e2947d535289b5f1776c5b887811f73
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-18 16:56:14 +0000

    switch statement

---
+++ 05_control_flow/switch.js
@@ -0,0 +1,32 @@
+const month=13
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    default:
+        console.log("Default case matched");
+        break;
+}
+
+// break keyword is used to break the flow of the execution 
