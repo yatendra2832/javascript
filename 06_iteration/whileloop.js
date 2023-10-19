76ec500abd214fca28820000468eb3e03763a7d3
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-19 06:14:54 +0000

    while and do while

---
+++ 06_iteration/whileloop.js
@@ -0,0 +1,22 @@
+// While loop & do-while loop
+// let i=0
+// while(i<10){
+//     console.log(`value of index is ${i}`);
+//    i=i+2
+// }
+
+// Reading the array from the while loop
+// let myArray=[12,24,36,48,60]
+// let arr=0
+// while(arr<myArray.length){
+//    console.log(`Value is ${myArray[arr]}`) 
+//    arr=arr+1
+// }
+
+// do while loop is atleast execute once whether the condition is true or false
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
+
