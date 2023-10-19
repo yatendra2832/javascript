0829224aa1a5ad298e95c366cb6d56183b7cdbab
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-19 05:56:16 +0000

    for loop

---
+++ 06_iteration/forloop.js
@@ -0,0 +1,34 @@
+// Loops
+/*for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element)
+}
+*/
+
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         const element = i+j
+//         console.log(element);
+//     }
+    
+// }
+// for one time of outer loop there is full cycle of the 
+
+// let myArray=["yatendra","sushant","sapna","neeraj"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)    
+// }
+// that will iterate through the array
+// Break and continue in the loops
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i==5){
+//         console.log('Detected 5')
+//         continue
+//     }
+//    console.log(i) 
+// //    if(i==10) break
+// }
+// break :it will break the flow of the execution of the for loop
+// continue: it will skip that particular condition case
