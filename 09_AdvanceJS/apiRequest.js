331bdd1e75d3b30f06f70a20af969bcf9fb42267
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-24 07:04:43 +0000

    Explainatin of the code

--- 09_AdvanceJS/apiRequest.js
+++ 09_AdvanceJS/apiRequest.js
@@ -18,3 +18,25 @@ xhr.onreadystatechange = function() {
   }
 };
 xhr.send();
+
+/*Here is explaination of the code that how it is working behind the scene
+1.The code starts with console.log('Hello'), which simply logs the message "Hello" to the console, indicating the program has 
+started.
+
+2.It sets the reqURL variable to the URL 'https://randomuser.me/api/', which is the endpoint of an API. This API provides random
+ user data when queried.
+
+3.It creates a new XMLHttpRequest object named xhr. The XMLHttpRequest object is used for making HTTP requests to a server.
+ In this case, it's configured to make a GET request to the URL specified in reqURL.
+
+4.The xhr.onreadystatechange event handler is set to a function. This function is called whenever the state of the XMLHttpRequest 
+object changes. When xhr.readyState equals 4, it means the request is complete.
+
+5.Inside the if (xhr.readyState === 4) block, it checks if the xhr.status is 200. A status code of 200 indicates that the request
+ was successful. If the status is not 200, it logs an error message.
+
+6.If the status is 200, it proceeds to parse the response. It uses JSON.parse(xhr.responseText) to convert the JSON response 
+from the API into a JavaScript object.
+
+7.Finally, it logs the parsed data object to the console. The data object should contain random user information.
+*/
