a913596713a4646b2f1fc31aff3c772176cfe6a9
Yatendra Singh <113310722+yatendra2832@users.noreply.github.com>
2023-10-29 07:05:50 +0000

    closure in JS

---
+++ 10_OOPs/closure.js
@@ -0,0 +1,24 @@
+/*function init(){
+    let name='yatendra singh'
+    function displayName(){
+        console.log(name);
+    }
+    displayName()
+}
+// init()
+*/
+/*
+function outer() {
+    let username = 'hitesh'
+    console.log(username);// that will print the name of the user when that function will be called
+    function inner(){
+        console.log('Inner :',username);
+    }
+
+}
+// console.log(username);// that will print the exception as the username is not accessible outside the function
+
+outer()
+*/
+// parent function varibales are accessible by the children but children valribales are not accessible by the parent function
+//   when we return a function then not only the function is returning but also the whole lexical scope of the function will return 
