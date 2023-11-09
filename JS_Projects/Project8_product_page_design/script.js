52d6899417c7b57ac4a2e017ac61d15ab14a6ccc
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-11-09 05:48:37 +0530

    JS: scripting of the color button size selection

---
+++ JS_Projects/Project8_product_page_design/script.js
@@ -0,0 +1,21 @@
+let productImg=document.getElementById("productImg");
+let btn = document.getElementsByClassName("btn");
+
+btn[0].onclick=function() {
+    productImg.src="/images/image1.png"
+    for (const bt of btn) {
+        bt.classList.remove("active");
+    }this.classList.add("active")
+}
+btn[1].onclick=function() {
+    productImg.src="/images/image2.png"
+    for (const bt of btn) {
+        bt.classList.remove("active");
+    }this.classList.add("active")
+}
+btn[2].onclick=function() {
+    productImg.src="/images/image3.png"
+    for (const bt of btn) {
+        bt.classList.remove("active");
+    }this.classList.add("active")
+}
