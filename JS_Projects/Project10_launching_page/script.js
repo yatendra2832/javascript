46ad52e2a360f1e734391edf875304e21480ec33
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-11-11 05:15:09 +0530

    JS:Scripting of the countdown and functionality

---
+++ JS_Projects/Project10_launching_page/script.js
@@ -0,0 +1,23 @@
+let countDownDate = new Date("Nov 16, 2023 00:00:00").getTime();
+let x = setInterval(() => {
+    let now =new Date().getTime();
+    let distance = countDownDate-now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+}, 1000);
+
