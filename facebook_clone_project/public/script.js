64ba9df31467730d1b9efa72622dfbdaba2f6394
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-11-25 18:56:57 +0530

    scripting of the theme mode button

--- facebook_clone_project/public/script.js
+++ facebook_clone_project/public/script.js
@@ -1,5 +1,33 @@
 let settingsMenu=document.querySelector(".settings-menu");
+let darkBtn=document.getElementById("dark-btn");
+
+
 function settingsMenuToggle(){
     settingsMenu.classList.toggle("settings-menu-height");
+
 }
+
+darkBtn.onclick=function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme","dark");
+    }else{
+        localStorage.setItem("theme","light");
+    }
+    
+}
+
+if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+
+localStorage.setItem("theme","dark");
+localStorage.getItem("theme");
 
