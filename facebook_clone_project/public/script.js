89066ff530074bb9509735282dca4aca83555128
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-11-28 08:25:25 +0530

    script for the seemore  by adding toggleclass

--- facebook_clone_project/public/script.js
+++ facebook_clone_project/public/script.js
@@ -28,6 +28,26 @@ else if(localStorage.getItem("theme") == "dark"){
     document.body.classList.add("dark-theme");
 }
 
-localStorage.setItem("theme","dark");
+localStorage.setItem("theme","light");
 localStorage.getItem("theme");
+
+// let seemore=document.getElementById("see-more-link");
+// function seeMore() {
+//     let content=document.getElementById("additional-content");
+//     if(content.style.display=="none"){
+//         content.style.display="block";
+//         seemore.innerHTML="See Less";
+//     }
+//     else{
+//         content.style.display="none";
+//         seemore.innerHTML="See More";
+//     }
+// }
+// More better way to implement the seemore fuctinality is to use toggle
+let seemore = document.getElementById("see-more-link");
+function seeMore() {
+    let content = document.getElementById("additional-content");
+    content.classList.toggle("show");
+    seemore.innerHTML = content.classList.contains("show") ? "See Less" : "See More";
+}
 
